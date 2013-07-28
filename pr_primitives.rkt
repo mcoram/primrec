44171e7d9b6a59118a2098154d5d18dce4b02347
Marc A. Coram <marc.coram@gmail.com>
2013-07-28 15:17:14 -0700

    Updated R0 and R1 to use iterative forms of primitive recursion -- they seem to be much more time/space efficient.

--- pr_primitives.rkt
+++ pr_primitives.rkt
@@ -13,20 +13,34 @@
 (define (P33 x y z) z)
 
 ; Primitive recursion operators
-(define (R0 f g) ;  primitive recursion on an arity 2 and arity 0 (tech. I present the args to f in reverse classic order)
+(define (R0-iter f g) ;  primitive recursion on an arity 2 and arity 0 (tech. I present the args to f in reverse classic order)
+  (lambda (x)
+    (let ([u g])
+      (for ([i (in-range 0 x)])
+        (set! u (f u i)))
+      u)))
+(define (R1-iter f g) ;  primitive recursion on an arity 2 and arity 0 (tech. I present the args to f in reverse classic order)
+  (lambda (x y)
+    (let ([u (g y)])
+      (for ([i (in-range 0 x)])
+        (set! u (f u i y)))
+      u)))
+(define (R0-rec f g) ;  primitive recursion on an arity 2 and arity 0 (tech. I present the args to f in reverse classic order)
   (letrec ([phi (lambda (x) 
                   (if (equal? x 0) 
                       g 
                       (let ([xm (- x 1)])
                         (f (phi xm) xm))))])
     phi))
-(define (R1 f g) ;  primitive recursion on an arity 3 and arity 1 (tech. I present the args to f in reverse classic order)
+(define (R1-rec f g) ;  primitive recursion on an arity 3 and arity 1 (tech. I present the args to f in reverse classic order)
   (letrec ([phi (lambda (x1 x2) 
                   (if (equal? x1 0) 
                       (g x2) 
                       (let ([x1m (- x1 1)])
                         (f (phi x1m x2) x1m x2))))]) 
     phi))
+(define R0 R0-iter)
+(define R1 R1-iter)
 
 ; Composition operators; Cij composes one arity i function with i arity j functions that all recieve the same j arguments so that the result of the composition is an arity j function
 (define (C10 f1 f2)
@@ -56,3 +70,17 @@
 
 ; This example constructs the doubling function and applies it to 17 via primitive recursion
 ; (C10 (R0 (C12 S (C12 S P21)) 0) 17) 
+
+
+; Speed test for the R0 and R1 operators; iter iter wins
+; c.f. Eulerian numbers: http://oeis.org/A000295
+;'(0 0 1 4 11 26 57 120 247 502 1013 2036 4083 8178 16369 32752 65519 131054 262125 524268)
+;(time (for/list ([x (in-range 20)]) ((R0-iter (R1-iter (C13 S (C13 S P31)) P11) 0) x)))
+;(time (for/list ([x (in-range 20)]) ((R0-rec (R1-rec (C13 S (C13 S P31)) P11) 0) x)))
+;(time (for/list ([x (in-range 20)]) ((R0-iter (R1-rec (C13 S (C13 S P31)) P11) 0) x)))
+;(time (for/list ([x (in-range 20)]) ((R0-rec (R1-iter (C13 S (C13 S P31)) P11) 0) x)))
+
+;> (time (for/list ([x (in-range 25)]) ((R0-iter (R1-iter (C13 S (C13 S P31)) P11) 0) x)))
+;cpu time: 4629 real time: 4627 gc time: 0
+;> (time (for/list ([x (in-range 25)]) ((R0-rec (R1-rec (C13 S (C13 S P31)) P11) 0) x)))
+;cpu time: 154281 real time: 161707 gc time: 77077
